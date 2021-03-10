e8ec7038a24d239129d5a12ebeb7625723fd4dc9
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-03-10 11:43:05 -0500

    Agregando el Logotipo #6

--- wp-content/themes/gymfitness/header.php
+++ wp-content/themes/gymfitness/header.php
@@ -6,7 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<header class="site_header">
-   <h1>Site Header</h1>
+    <header class="site_header">
+        <div class="contenedor">
+        <div class="barra-navegacion">
+            <div class="logo">
+                <img src="<?php echo get_template_directory_uri(); ?>\img\logo.svg" alt= "logo site">
+            </div>
+            <nav>
+              <!-- Navegacion aqui  -->
+            </nav>
+        </div>
+        </div>
+
+  
    
    </header> 
