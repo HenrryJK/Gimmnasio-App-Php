776dcabb5d1fba7c561d96abc0ee08a437bb2453
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-03-11 10:28:40 -0500

    Agregando otras hojas de estilos #9

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -11,7 +11,13 @@ add_action('init' , 'gymfitness_menus' );
 
 //Scrips y Styles
 function gymfitness_scripts_styles() {
-    wp_enqueue_style('styles', get_stylesheet_uri(), array(), '1.0.0');
+    wp_enqueue_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
+
+    wp_enqueue_style('googleFont','https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Raleway:wght@100&family=Staatliches&display=swap',array(), '1.0.0');
+   
+   
+        /// hoja de estilos 
+      wp_enqueue_style('styles', get_stylesheet_uri(), array('normalize'), '1.0.0');
 }
 
 add_action( 'wp_enqueue_scripts' , 'gymfitness_scripts_styles' );
