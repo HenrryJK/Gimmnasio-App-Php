de90b5a9d58b883d978782317d96268dfb366f97
HenrryJK <henrryorbezo@upeu.edu.pe>
2021-03-17 14:03:10 -0500

    Que son los Custom Post Types y creando un Plugin #25

---
+++ wp-content/plugins/gymfitness_post_types/gymfitness_post_types.php
@@ -0,0 +1,15 @@
+<?php 
+/* 
+    Plugin Name: Gym Fitness - Post Types
+    Plugin URI: 
+    Description: Añade Post Types al Sitio Gym Fitness
+    Version: 1.0.0
+    Author: Henrry Jean Paul Orbezo Perkga
+    Author URI:http://twitter.com
+    Text Domain: gymfitness
+
+
+
+
+*/
+?>
